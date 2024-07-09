c511d835c8ef48af826f4a12f03fae9b32aa9c36
oneto1advice <oneto1advice@gmail.com>
2024-07-09 13:15:10 +0530

    ddf

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use('/api', userRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
